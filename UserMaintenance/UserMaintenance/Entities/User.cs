f67d9a378365cea05d22f31827a5b1f2987f1df7
Lukács Marton <lukacs.marton@stud.uni-corvinus.hu>
2022-09-30 12:26:09 +0200

    resourcenevek

---
+++ UserMaintenance/UserMaintenance/Entities/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserMaintenance.Entities
+{
+    class User
+    {
+        public Guid ID { get; set; } = Guid.NewGuid();
+        public string Keresztnev { get; set; }
+        public string Vezeteknev { get; set; }
+        public string TeljesNev
+        {
+            get
+            {
+                return string.Format(
+                    "{0} {1}",
+                    Keresztnev,
+                    Vezeteknev);
+            }
+        }
+    }
+}
