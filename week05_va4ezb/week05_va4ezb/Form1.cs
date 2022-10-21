09123d4f04723640cf7d08b2638600134262340b
Lukács Marton <lukacs.marton@stud.uni-corvinus.hu>
2022-10-21 13:00:24 +0200

    7.

--- week05_va4ezb/week05_va4ezb/Form1.cs
+++ week05_va4ezb/week05_va4ezb/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 using week05_va4ezb.Entities;
 using week05_va4ezb.mnbservicereference;
@@ -19,17 +20,33 @@ namespace week05_va4ezb
         public Form1()
         {
             InitializeComponent();
+
+            
+            refresh();
+
+        }
+
+        private void refresh()
+        {
+            Rates.Clear();
+            dataGridView1.DataSource = Rates;
+            lekerd();
+            diagram();
+        }
+        private string lekerd()
+        {
             var mnbService = new MNBArfolyamServiceSoapClient();
             var request = new GetExchangeRatesRequestBody()
             {
-                currencyNames = "EUR",
-                startDate = "2020-01-01",
-                endDate = "2020-06-30"
+                currencyNames = comboBox1.Text,
+                startDate = dateTimePicker1.Value.ToString(),
+                endDate = dateTimePicker2.Value.ToString()
             };
             var response = mnbService.GetExchangeRates(request);
             var result = response.GetExchangeRatesResult;
 
             dataGridView1.DataSource = Rates;
+
 
             var xml = new XmlDocument();
             xml.LoadXml(result);
@@ -49,9 +66,49 @@ namespace week05_va4ezb
 
 
 
-                Rates.Add(rate);
+                Rates.Add(rate);    
+            }
+            return result;
+        }
+        private void xml()
+            {           
             }
 
+
+            
+        private void diagram()
+            {
+            chartRateData.DataSource = Rates;
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+            var chartarea = chartRateData.ChartAreas[0];
+            chartarea.AxisX.MajorGrid.Enabled = false;
+            chartarea.AxisY.MajorGrid.Enabled = false;
+            chartarea.AxisY.IsStartedFromZero = false;
+
+
+
+
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            refresh();
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            refresh();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            refresh();
         }
     }
 }
