07157461085ec46df3f9b3940d5ca355b33a9b9d
Lukács Marton <lukacs.marton@stud.uni-corvinus.hu>
2022-10-21 12:22:34 +0200

    5.

---
+++ week05_va4ezb/week05_va4ezb/Entities/RateData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week05_va4ezb.Entities
+{
+    class RateData
+    {
+
+        public DateTime Date { get; set; }
+        public string Currency { get; set; }
+        public decimal value { get; set; }
+    }
+}
--- week05_va4ezb/week05_va4ezb/Form1.cs
+++ week05_va4ezb/week05_va4ezb/Form1.cs
@@ -7,14 +7,51 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
+using week05_va4ezb.Entities;
+using week05_va4ezb.mnbservicereference;
 
 namespace week05_va4ezb
 {
     public partial class Form1 : Form
     {
+        BindingList<RateData> Rates = new BindingList<RateData>();
         public Form1()
         {
             InitializeComponent();
+            var mnbService = new MNBArfolyamServiceSoapClient();
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+            var response = mnbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
+
+            dataGridView1.DataSource = Rates;
+
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                var rate = new RateData();
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+
+                var child = (XmlElement)element.ChildNodes[0];
+                rate.Currency = child.GetAttribute("curr");
+
+                var unit = decimal.Parse(child.GetAttribute("unit"));
+                var value = decimal.Parse(child.InnerText);
+                if (unit != 0)
+                    rate.value = value / unit;
+
+
+
+                Rates.Add(rate);
+            }
+
         }
     }
 }
