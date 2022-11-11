58baf5d8a4d48f29ab591bf16403d3c9a937e353
Lukács Marton <lukacs.marton@stud.uni-corvinus.hu>
2022-11-11 12:48:49 +0100

    SAVEFILE

--- week07_va4ezb/week07_va4ezb/Form1.cs
+++ week07_va4ezb/week07_va4ezb/Form1.cs
@@ -3,18 +3,89 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using week07_va4ezb.Entities;
 
 namespace week07_va4ezb
 {
     public partial class Form1 : Form
     {
+        PortfolioEntities context = new PortfolioEntities();
+        List<Tick> Ticks;
+        List<PortfolioItem> portfolioItems = new List<PortfolioItem>();
         public Form1()
         {
             InitializeComponent();
+            Ticks = context.Tick.ToList();
+            dataGridView1.DataSource = Ticks;
+            CreatePortfolio();
+
+            List<decimal> Nyereségek = new List<decimal>();
+            int intervalum = 30;
+            DateTime kezdőDátum = (from x in Ticks select x.TradingDay).Min();
+            DateTime záróDátum = new DateTime(2016, 12, 30);
+            TimeSpan z = záróDátum - kezdőDátum;
+            for (int i = 0; i < z.Days - intervalum; i++)
+            {
+                decimal ny = GetPortfolioValue(kezdőDátum.AddDays(i + intervalum))
+                           - GetPortfolioValue(kezdőDátum.AddDays(i));
+                Nyereségek.Add(ny);
+                Console.WriteLine(i + " " + ny);
+            }
+
+            var nyereségekRendezve = (from x in Nyereségek
+                                      orderby x
+                                      select x)
+                                        .ToList();
+            MessageBox.Show(nyereségekRendezve[nyereségekRendezve.Count() / 5].ToString());
+        }
+        public void CreatePortfolio()
+        {
+            portfolioItems.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
+            portfolioItems.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            portfolioItems.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
+
+            dataGridView2.DataSource = portfolioItems;
+        }
+        private decimal GetPortfolioValue(DateTime date)
+        {
+            decimal value = 0;
+            foreach (var item in portfolioItems)
+            {
+                var last = (from x in Ticks
+                            where item.Index == x.Index.Trim()
+                               && date <= x.TradingDay
+                            select x)
+                            .First();
+                value += (decimal)last.Price * item.Volume;
+            }
+            return value;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Title = "Hova szeretné menteni a listát?";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamWriter sw = new StreamWriter(sfd.FileName, false, Encoding.UTF8))
+                {
+                    sw.WriteLine("Időszak" + ";"+"Nyerség");
+                    foreach (var p in Ticks)
+                    {
+                        sw.Write(p.TradingDay.ToString());
+                        sw.Write(";");
+                        sw.Write(p.Price.ToString());
+                        sw.WriteLine();
+                    }
+                  
+                }
+                
+            }
         }
     }
 }
